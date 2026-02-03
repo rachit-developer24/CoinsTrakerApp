608737158a95387e1855b7cfcecca7c76fcfef62
rachit-developer24 <srachit841@gmail.com>
2026-02-03 15:37:17 +0000

    Initial Commit

---
+++ CoinsPriceTracker/CoinsPriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsPriceTrackerApp.swift
+//  CoinsPriceTracker
+//
+//  Created by Rachit Sharma on 03/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CoinsPriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
